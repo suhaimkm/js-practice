4ca1eebbc72f13d078f4642177ced37a0bc1727e
suhaim km <suhaimkm441@gmail.com>
2024-12-31 11:04:09 +0530

    31-12-2024 update

---
+++ factreverse.js
@@ -0,0 +1,14 @@
+
+let ans = fact(5);
+console.log(ans);
+
+function fact(n) {
+    let factorial = 1;
+    for (let i = n; i<= n; i--) {
+        console.log(i);
+        factorial = factorial * i;
+        
+    }
+    return factorial;
+}
+
