6276e5ee1d607da1ff4b2c8e5c813ade7cac2045
suhaim km <suhaimkm441@gmail.com>
2025-01-05 11:26:46 +0530

    changes

--- assignment/assign2.js
+++ assignment/assign2.js
@@ -1,5 +1,5 @@
-let numbers = [10, 20, 30, 40, 50];
+let numbers = [10, 20, 1,3,9,30, 40, 50];
 for (const odd of numbers) {
     if (odd % 2 !== 0) 
-        console.log(`odd numbers : ${odd}`);          
+        console.log(`odd number : ${odd}`);          
 }
