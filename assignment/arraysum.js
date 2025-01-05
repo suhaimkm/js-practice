7bc0570c24ad8af9157c20eaafd60476f0fb1869
suhaim km <suhaimkm441@gmail.com>
2025-01-05 11:25:13 +0530

    assignment

---
+++ assignment/arraysum.js
@@ -0,0 +1,7 @@
+let numbers = [10, 20, 30, 40, 50];
+sum = 0;
+numbers.forEach((num, i) => {
+    sum += num;
+});
+console.log(`sum =${sum}`);
+
---
+++ assignment/assign2.js
@@ -0,0 +1,5 @@
+let numbers = [10, 20, 30, 40, 50];
+for (const odd of numbers) {
+    if (odd % 2 !== 0) 
+        console.log(`odd numbers : ${odd}`);          
+}
