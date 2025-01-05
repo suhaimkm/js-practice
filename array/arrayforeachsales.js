a43d85c1f796db0ff56d70a61b24dc796f296619
suhaim km <suhaimkm441@gmail.com>
2025-01-05 11:09:49 +0530

    changes

--- array/arrayforeachsales.js
+++ array/arrayforeachsales.js
@@ -2,5 +2,6 @@ let sales = [100, 200, 300, 400, 500, 600];
 totalsales = 0;
 sales.forEach((sales, i) => {
     console.log(`sales of day ${i + 1} is ${sales}`);
-    totalsales += sales;});
-    console.log(`total sales is ${totalsales}`);
+    totalsales += sales;
+});
+console.log(`total sales is ${totalsales}`);
