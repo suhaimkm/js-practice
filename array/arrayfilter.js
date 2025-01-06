d50a2d64ed92f179389031ad4b4bb9d2baf74380
suhaim km <suhaimkm441@gmail.com>
2025-01-06 10:12:31 +0530

    newfunctions

---
+++ array/arrayfilter.js
@@ -0,0 +1,3 @@
+let numbers = [ 80 , 90, 40, 15, 85] ;
+numbers2 = numbers.filter(n => n > 50);
+console.log(numbers2);
---
+++ array/arrayreduce.js
@@ -0,0 +1,3 @@
+let numbers = [1, 2, 3, 4, 5];
+numbers2 = numbers.reduce( (n ,total) => n + total , 0);
+console.log(numbers2);
